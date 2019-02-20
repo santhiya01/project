23680ef6be0f4bfd53b52570d42a49511120be8f
santhiya01 <43370785+santhiya01@users.noreply.github.com>
2019-02-20 14:52:04 +0530

    Create program01.py

---
+++ program01.py
@@ -0,0 +1,5 @@
+number =float(input("enter the number");
+if(number >0);
+print("enter the number is positive".format(number));
+else(number <0);
+print("enter the number is nagetive".format(number))
